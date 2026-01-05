029ca278ba981571bb17142678f1026f3c17fe8f
nanedb <giovanni.dalbo@gmail.com>
2026-01-05 20:14:21 +0100

    Prima di passare al formato "Pages"

---
+++ Views/SetupPage.xaml.cs
@@ -0,0 +1,206 @@
+﻿using Foscamun2026.Data;
+using Foscamun2026.Models;
+using Foscamun2026.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Foscamun2026
+{
+    public partial class SetupPage : Page
+    {
+        public event Action RequestClose;
+
+        public SetupPage()
+        {
+            InitializeComponent();
+
+            // MVVM: collega il ViewModel
+            DataContext = new CommitteeListViewModel(new SqliteDataAccess());
+            var VM = (CommitteeListViewModel)DataContext;
+
+            // Carica i comitati
+            _ = VM.LoadCommitteesAsync();
+
+            // Imposta la lingua salvata
+            string lang = Properties.Settings.Default.Lang;
+
+            switch (lang)
+            {
+                case "fr-FR":
+                    FraLangButton.IsChecked = true;
+                    break;
+
+                case "es-ES":
+                    EspLangButton.IsChecked = true;
+                    break;
+
+                default:
+                    EngLangButton.IsChecked = true;
+                    break;
+            }
+
+            // Ordina la lista
+            CommitteesListBox.Items.SortDescriptions.Clear();
+            CommitteesListBox.Items.SortDescriptions.Add(
+                new SortDescription("Name", ListSortDirection.Ascending));
+
+        }
+
+        // ------------------------------------------------------------
+        // CAMBIO LINGUA (NON DISTRUGGE GLI STILI GLOBALI)
+        // ------------------------------------------------------------
+        private void ChangeLanguage(string culture)
+        {
+            // Imposta la cultura del thread
+            var cultureInfo = CultureInfo.GetCultureInfo(culture);
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            // Trova il dizionario Strings attuale
+            var oldDict = Application.Current.Resources.MergedDictionaries
+                .FirstOrDefault(d => d.Source != null &&
+                                     d.Source.OriginalString.Contains("Strings"));
+
+            // Crea il nuovo dizionario
+            var newDict = new ResourceDictionary
+            {
+                Source = culture switch
+                {
+                    "fr-FR" => new Uri("Strings/Strings.fr.xaml", UriKind.Relative),
+                    "es-ES" => new Uri("Strings/Strings.es.xaml", UriKind.Relative),
+                    _ => new Uri("Strings/Strings.en.xaml", UriKind.Relative)
+                }
+            };
+
+            // Sostituisci SOLO il dizionario delle stringhe
+            if (oldDict != null)
+            {
+                int index = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                Application.Current.Resources.MergedDictionaries[index] = newDict;
+            }
+            else
+            {
+                Application.Current.Resources.MergedDictionaries.Add(newDict);
+            }
+
+            // Salva preferenza
+            Properties.Settings.Default.Lang = culture;
+            Properties.Settings.Default.Save();
+        }
+
+        // Eventi RadioButton
+        private void EngLangButton_Checked(object sender, RoutedEventArgs e) => ChangeLanguage("en-US");
+        private void FraLangButton_Checked(object sender, RoutedEventArgs e) => ChangeLanguage("fr-FR");
+        private void EspLangButton_Checked(object sender, RoutedEventArgs e) => ChangeLanguage("es-ES");
+
+
+        // ------------------------------------------------------------
+        // SELEZIONE COMITATO
+        // ------------------------------------------------------------
+        private void CommitteesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CommitteesListBox.SelectedItem is Committee selected)
+            {
+                Properties.Settings.Default.SelCommID = selected.CommID;
+                Properties.Settings.Default.SelCommName = selected.Name;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        // ------------------------------------------------------------
+        // AGGIUNGI COMITATO
+        // ------------------------------------------------------------
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            //var addWindow = new AddCommitteeWindow { Owner = this };
+
+            //this.Hide();
+            //bool? result = addWindow.ShowDialog();
+            //this.Show();
+
+            //if (result == true && DataContext is CommitteeListViewModel vm)
+            //    _ = vm.LoadCommitteesAsync();
+        }
+
+        // ------------------------------------------------------------
+        // MODIFICA COMITATO
+        // ------------------------------------------------------------
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            //if (CommitteesListBox.SelectedItem is not Committee selected)
+            //{
+            //    MessageBox.Show("Select a committee first.");
+            //    return;
+            //}
+
+            //var editWindow = new EditCommitteeWindow(selected) { Owner = this };
+
+            //this.Hide();
+            //bool? result = editWindow.ShowDialog();
+            //this.Show();
+
+            //if (result == true && DataContext is CommitteeListViewModel vm)
+            //    _ = vm.LoadCommitteesAsync();
+        }
+
+        // ------------------------------------------------------------
+        // RIMUOVI COMITATO
+        // ------------------------------------------------------------
+        private async void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CommitteesListBox.SelectedItem is not Committee selected)
+            {
+                MessageBox.Show("Select a committee first.");
+                return;
+            }
+
+            if (MessageBox.Show($"Remove committee '{selected.Name}'?",
+                "Foscamun", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                var data = new SqliteDataAccess();
+                await data.RemoveCommitteeAsync(selected);
+
+                if (DataContext is CommitteeListViewModel vm)
+                    _ = vm.LoadCommitteesAsync();
+            }
+        }
+
+        // ------------------------------------------------------------
+        // NAVIGAZIONE
+        // ------------------------------------------------------------
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            //if (CommitteesListBox.SelectedItem is not Committee selected)
+            //{
+            //    MessageBox.Show("Select a committee first.");
+            //    return;
+            //}
+
+            //Window next = selected.Name == "ICJ"
+            //    ? new ICJStartWindow { Owner = this.Owner }
+            //    : new StartWindow { Owner = this.Owner };
+
+            //Properties.Settings.Default.Save();
+
+            //this.Hide();
+            //next.ShowDialog();
+            //this.Show();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            RequestClose?.Invoke();
+
+            if (NavigationService?.CanGoBack == true)
+                NavigationService.GoBack();
+        }
+
+        //private void Window_Closing(object sender, CancelEventArgs e)
+        //{
+        //    Owner?.Show();
+        //}
+    }
+}
--- Views/WelcomeWindow.xaml.cs
+++ Views/WelcomeWindow.xaml.cs
@@ -1,14 +1,51 @@
-﻿using Foscamun2026.ViewModels;
+﻿using System.ComponentModel;
 using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Foscamun2026.Views
 {
-    public partial class WelcomeWindow : Window
+    public partial class WelcomeWindow : Window, INotifyPropertyChanged
     {
+        private bool _isPaneCollapsed = false;
+        public bool IsPaneCollapsed
+        {
+            get => _isPaneCollapsed;
+            set
+            {
+                if (_isPaneCollapsed == value) return;
+                _isPaneCollapsed = value;
+                OnPropertyChanged(nameof(IsPaneCollapsed));
+            }
+        }
+
         public WelcomeWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        // Handler chiamato dal Click dell'hamburger (definito in XAML)
+        private void HamburgerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TogglePane();
+        }
+
+        private void TogglePane()
+        {
+            const double expandedWidth = 150;
+            const double collapsedWidth = 56;
+
+            if (IsPaneCollapsed)
+            {
+                LeftColumn.Width = new GridLength(expandedWidth);
+                IsPaneCollapsed = false;
+            }
+            else
+            {
+                LeftColumn.Width = new GridLength(collapsedWidth);
+                IsPaneCollapsed = true;
+            }
         }
 
         private void ExitBtn_Click(object sender, RoutedEventArgs e)
@@ -18,6 +55,12 @@ namespace Foscamun2026.Views
 
         private void ConfigureBtn_Click(object sender, RoutedEventArgs e)
         {
+            // Forza lo stato Normal sul pulsante che è stato cliccato
+            if (sender is Control clickedCtrl)
+            {
+                VisualStateManager.GoToState(clickedCtrl, "Normal", true);
+            }
+
             // Nascondi la finestra corrente
             this.Hide();
 
@@ -27,10 +70,12 @@ namespace Foscamun2026.Views
             {
                 // Quando SetupWindow viene chiusa, riapri WelcomeWindow
                 this.Show();
+
+                // Ripristina lo stato Normal su tutti i pulsanti della finestra (evita hover "bloccati")
+                //ResetAllMenuButtonsToNormal();
             };
             setup.ShowDialog();
         }
-
         private void NextBtn_Click(object sender, RoutedEventArgs e)
         {
             //var app = Application.Current as App;
@@ -40,5 +85,31 @@ namespace Foscamun2026.Views
             //    return;
             //}
         }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        // Ripristina lo stato Normal su tutti i Button nella finestra
+        //private void ResetAllMenuButtonsToNormal()
+        //{
+        //    foreach (var btn in FindVisualChildren<Button>(this))
+        //    {
+        //        // Se vuoi limitare ai soli pulsanti con uno specifico Style, decommenta e adatta:
+        //        // if (btn.Style == (Style)FindResource("MenuButtonTest"))
+        //        VisualStateManager.GoToState(btn, "Normal", true);
+        //    }
+        //}
+
+        // Helper per enumerare la visual tree
+        //private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        //{
+        //    if (depObj == null) yield break;
+        //    for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+        //    {
+        //        var child = VisualTreeHelper.GetChild(depObj, i);
+        //        if (child is T t) yield return t;
+        //        foreach (var childOfChild in FindVisualChildren<T>(child)) yield return childOfChild;
+        //    }
+        //}
     }
 }
